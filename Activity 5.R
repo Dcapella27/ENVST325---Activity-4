37283d64e01f6002b3cb2cc73d616200881c266e
Dcapella27 <dcapella@hamilton.edu>
2022-03-10 16:45:24 +0000

    Activity 4 end of class

--- Activity 5.R
+++ Activity 5.R
@@ -7,14 +7,16 @@ library(PerformanceAnalytics)
 # Read in data
 ghg <- read.csv("/cloud/project/activity05/Deemer_GHG_Data.csv")
 
-# Transform ch4 using log function
+# Restructure data
 ghg$log.ch4 <- log(ghg$ch4 + 1)
 ghg$log.age <- log(ghg$age)
-ghg$log.DIP <- log(ghg$DIP+1)
+ghg$log.DIP <- log(ghg$DIP + 1)
 ghg$log.precip <- log(ghg$precipitation)
 ghg$log.SA <- log(ghg$surface.area)
-ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
-ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
+ghg$BorealV <- ifelse(ghg$Region == "Boreal", 1, 0)
+ghg$TropicalV <- ifelse(ghg$Region == "Tropical", 1, 0)
+
+# Fit full linear regression model 
 mod.full <- lm(log.ch4 ~ airTemp +
                  log.age + mean.depth +
                  log.DIP +
@@ -22,3 +24,7 @@ mod.full <- lm(log.ch4 ~ airTemp +
                  BorealV, 
                  data=ghg)
 summary(mod.full)
+
+# Fit forward stepwise regression model with AIC
+full.step <- ols_step_forward_aic(mod.full)
+full.step
